537433ce3acf878dcbd04799239f05cb909f0659
Rioxcell <rioxcellcrespocm@gmail.com>
2026-01-08 19:51:46 +0100

    proyecto final actualizado

--- js/funciones.js
+++ js/funciones.js
@@ -1,15 +1,26 @@
+//-------------------------------------------------------
+//                elementos del dom
+//-------------------------------------------------------
+
 const boton = document.querySelector(".hamburguesa");
 const menu = document.querySelector(".menu");
 const carrusel = document.querySelector(".contenedorcarrusel");
 const btnsig = document.querySelector(".siguiente");
 const btnant = document.querySelector(".atras");
 
-
+//----------------------------------------------------
+//               menu hamburguesa
+//----------------------------------------------------
+//cuando se esta en movil, al darle click al boton hamburguesa cambia el icono
 boton.addEventListener("click", () => {
-    const abierto = menu.classList.toggle("activa");
+    const abierto = menu.classList.toggle("activo");
     boton.textContent = abierto ? "✖" : "☰";
 });
 
+//------------------------------------------------------
+//              Funciones del carrusel
+//------------------------------------------------------
+//calcula cuanto debe deslazarse el carrusel(distancia) ANCHO CARD + GAP
 function desplazamiento() {
     const card = carrusel.children[0];
     const gap = parseFloat(
@@ -19,30 +30,35 @@ function desplazamiento() {
     return card.offsetWidth + gap;
 };
 
+//Marca visualmente la card centarl como activa
 function actualizarcard() {
     const cards = carrusel.children;
-
+//quita la clase activa a todos
     Array.from(cards).forEach(card => {
         card.classList.remove("activa");
     });
+//activa la card central en la posicion 2
     if(cards[2]) {
         cards[2].classList.add("activa");
     }
 };
-
+//---------------------------------------------------------
+//                Boton siguiente
+//---------------------------------------------------------
+//Mueve el carrusel hacia la izquierda
 btnsig.addEventListener("click", () =>{
     const d = desplazamiento();
-
+//Animacion del desplazamiento
     carrusel.style.transition = "transform 0.45s ease";
     carrusel.style.transform = `translateX(-${d}px)`;
-
+//Reorganiza los elementos para el efecto infinito
     setTimeout(() => {
         carrusel.style.transition = "none";
 
         carrusel.appendChild(carrusel.firstElementChild);
 
         carrusel.style.transform = "translateX(0)";
-
+//Fuerza reflow para reiniciar transicion
         carrusel.offsetHeight;
 
         carrusel.style.transition = "transform 0.45s ease";
@@ -50,21 +66,20 @@ btnsig.addEventListener("click", () =>{
         actualizarcard();
     }, 450);
 });
-
+//Mueve el carrusel hacia la derecha
 btnant.addEventListener("click", () => {
     const d = desplazamiento();
-
+    //reocoloca la ultima card al inicio sin animacion
     carrusel.style.transition = "none";
-
     carrusel.insertBefore(
         carrusel.lastElementChild,
         carrusel.firstElementChild
     );
-
+    //coloca el carrusel desplazado
     carrusel.style.transform = `translateX(-${d}px)`;
-
+    //Fuerza reflow
     carrusel.offsetHeight;
-
+    //aplica animacion de regreso
     carrusel.style.transition = "transform 0.45s ease";
     carrusel.style.transform = "translateX(0)";
 
@@ -72,5 +87,7 @@ btnant.addEventListener("click", () => {
         actualizarcard();
     }, 450);
 });
+
+//Desde aqui inicia, marca la card central al cargar pagina
 
 actualizarcard();
